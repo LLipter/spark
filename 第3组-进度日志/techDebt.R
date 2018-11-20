4be49f4a5a25510640163d041deae068fc97befb
LLipter <katsudonn@163.com>
2018-11-20 13:22:50 +0800

    figure script

--- 第3组-进度日志/techDebt.R
+++ 第3组-进度日志/techDebt.R
@@ -1,16 +1,16 @@
 
-png(file="tech_debt.png",width=3000,height=3000,res=600,pointsize=8)
-colsmap <- rainbow(3000,start=0,end=0.5)
+png(file="tech_debt.png",width=3000,height=3000,res=600,pointsize=6)
+colsmap <- heat.colors(2600)
 numbers <- c(432,599,153,125,2511,6,58)
 names(numbers) <- c("Complexity","Style","Compatibility","Performance","Maintainability","Accessibility","Security")
 cols <- NULL
 for (number in numbers){
-    cols <- append(cols, colsmap[3000-number])
+    cols <- append(cols, colsmap[2600-number])
 }
 barplot(numbers,
-        border=0,
+        cex.main=2,
         main="Tech Debt in Spark",
         ylab="Number of Issues",
         col=cols,
-        cex.names=0.7)
+        cex.names=1)
 dev.off()
